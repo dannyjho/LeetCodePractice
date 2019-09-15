99170dc67bb6966d0317732c951b996173abe267
dannyjho <dannyjho@simbo.com.tw>
2019-09-15 21:38:32 +0800

    Revert "sort code"
    This reverts commit 2b029dc8f53fb270a495b431ed066df95bda720e.

--- src/main/java/ZigZagConversion/ZigZagConversion.java
+++ src/main/java/ZigZagConversion/ZigZagConversion.java
@@ -8,19 +8,16 @@ public class ZigZagConversion {
         if (numRows == 1) {
             return s;
         }
+
         int length = s.length();
         int steps = 2 * numRows - 2;
         String result = "";
         for (int i = 0; i < numRows; ++i) {
             for (int j = 0; j < length; j += steps) {
                 int p = j + i;
-                if (p < length) {
-                    result += s.charAt(p);
-                    p = j + steps - i;
-                }
-                if (i != numRows - 1 && i != 0 && p < length) {
-                    result += s.charAt(p);
-                }
+                if (p < length) result += s.charAt(p);
+                p = j + steps - i;
+                if (i != numRows - 1 && i != 0 && p < length) result += s.charAt(p);
             }
         }
         return result;
