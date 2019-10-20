5004f83c4457732b5a5affe264b0a4c057d777f6
dannyjho <dannyjho@simbo.com.tw>
2019-10-20 21:24:28 +0800

    add threeSum logic and test

---
+++ src/test/java/ThreeSum/ThreeSumTest.java
@@ -0,0 +1,28 @@
+package ThreeSum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreeSumTest {
+    @Test
+    public void when_input_then_should_return_size_2_list() {
+        int[] input = new int[]{-1, 0, 1, 2, -1, -4};
+        ThreeSum threeSum = new ThreeSum();
+        List<List<Integer>> lists = threeSum.sumThree(input);
+        List<Integer> list1 = new ArrayList();
+        List<Integer> list2 = new ArrayList();
+        list1.add(-1);
+        list1.add(0);
+        list1.add(1);
+        list2.add(-1);
+        list2.add(-1);
+        list2.add(2);
+        List<List<Integer>> expectedResult = new ArrayList<List<Integer>>();
+        expectedResult.add(list2);
+        expectedResult.add(list1);
+        Assert.assertEquals(expectedResult,lists);
+    }
+}
